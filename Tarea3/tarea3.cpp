ea05f121c711c0e0f716a83d00ef919d0bfdfab1
kamus1 <benjamincamus9@gmail.com>
2022-07-01 01:50:54 -0400

    struct

--- Tarea3/tarea3.cpp
+++ Tarea3/tarea3.cpp
@@ -23,6 +23,51 @@ inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
     return ltrim(rtrim(s, t), t);
 }
 
+
+//Funcion return Numero cantidad de opciones
+int NumOpciones(string num){
+    string texto;
+    int contador = 0 ;
+    ifstream archivo;
+    archivo.open("input.txt", ios::in);
+    while(!archivo.eof()){
+        getline(archivo, texto); 
+        trim(texto);
+        if(texto == num){
+            contador++;
+        }
+    }
+    archivo.close();
+    return contador;
+}
+//Funcion para insertar salas
+void InsertarSala(struct sala *&primeraSala, string descripcion){
+
+}
+
+//Funcion para insertar Opciones
+void InsertarOpcion(){
+
+}
+
+
+//struct para las ocpiones o caminos
+struct opcion{
+    string descripcion, numOpcion;
+
+    struct opcion *siguiente;
+    struct sala *salaSiguiente;
+};
+
+//struct para las salas
+struct sala{
+    string descripcion, numSala;
+    int numOpciones;
+    
+    struct opcion *listaSalas;
+};
+
+
 int main(){
     
     string texto, nodoOrigen, decision, decripcion;
@@ -53,6 +98,8 @@ int main(){
 
     }
 
+    cout<<NumOpciones("6")<<endl;
+    
     archivo.close();
     return 1;
 }
